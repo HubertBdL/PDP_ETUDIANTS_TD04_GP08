9c94d8da7b8a57012f5d3fd6b1c093f6d5c13e2b
HubertBdL <hbiard33@gmail.com>
2023-01-17 10:08:11 +0100

    etape_3

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -11,39 +11,21 @@
 DHT_Unified dht(SENSOR, DHTTYPE);
 
 void setup() {
+
+
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
   Serial.begin(9600);
+
   // Initialize device.
   dht.begin();
   Serial.println(F("DHTxx Unified Sensor Example"));
+
   // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-}
 
-void loop() {
-  // Effectuer une mesure toutes les 5s
-  delay(5000);
   // Afficher l'humidité relative avec un peu de formatage
   sensors_event_t event;
   dht.temperature().getEvent(&event);
@@ -65,4 +47,11 @@ void loop() {
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   }
+
+  delay(100); 
+  esp_sleep_enable_timer_wakeup(5000000); 
+  esp_deep_sleep_start;
+
 }
+
+void loop(){}
